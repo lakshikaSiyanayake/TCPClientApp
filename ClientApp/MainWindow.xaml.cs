5c665a1dbdcd2f7334015dd6145edd4ad1a1fa6a
Lakshika Siyanayake <lakshika.siyanayake@gmail.com>
2022-01-02 10:24:37 +1100

    Add project files.

---
+++ ClientApp/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClientApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+        public void receiveMessage()
+        {
+            IPEndPoint ip = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9999);
+
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                server.Connect(ip);
+            }
+            catch (SocketException)
+            {
+                lblError.Content = "Unable to connect to server.";               
+            }
+
+            byte[] data = new byte[1024];
+            int receivedDataLength = server.Receive(data);
+            string stringData = Encoding.ASCII.GetString(data, 0, receivedDataLength);
+            txtMsg.Text = stringData.ToString();
+            server.Shutdown(SocketShutdown.Both);
+            server.Close();
+        }
+        private void btnReceive_Click(object sender, RoutedEventArgs e)
+        {
+            receiveMessage();
+        }
+    }
+}
